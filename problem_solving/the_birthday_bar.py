380b8de1a9f6f938a29b21200412f2fdb3384169
Mohit Jain <reachoutmohit@gmail.com>
2022-02-10 00:51:03 +0530

    solution : the birthday bar

---
+++ problem_solving/the_birthday_bar.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    choc_divs = 0
+    num_sub_arrs = len(s) - m + 1
+    sub_arrs = []
+
+    for i in range(0, num_sub_arrs):
+        if sum(s[i:m+i]) == d:
+            choc_divs += 1
+
+    return choc_divs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
